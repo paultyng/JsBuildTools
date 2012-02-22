71c305bb9ada8c326f0ef0764a6484f49027a8df
Paul Tyng <paul@paultyng.net>
2012-02-22 06:41:04 -0500

    Updates for restart enabling

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -1,6 +1,6 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("0.3.0.*")]
+[assembly: AssemblyVersion("0.3.1.*")]
 [assembly: AssemblyCompany("Paul Tyng")]
 [assembly: AssemblyProduct("JsBuildTools")]
 [assembly: AssemblyCopyright("Copyright © 2012 Paul Tyng")]
